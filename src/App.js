03029f765c43f140b043037bcbae991aaed6f87c
HanlenGit <94447216+HanlenGit@users.noreply.github.com>
2022-04-05 19:50:42 +0800

    App.js
    initial commit for App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import{ useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [message, setMessage] = useState(
+    "JavaScript is the best language in the world"
   );
+
+  // actions
+  function handleClick() {
+    setMessage("I am a BOSS! at JavaScript");
+  }
+
+  // UI
+  return (
+  <div>
+    <h1>{message}</h1>
+    <button onClick={handleClick}>Update the message!</button>
+    </div>
+    );
 }
 
 export default App;
